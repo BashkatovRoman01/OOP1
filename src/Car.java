68f6eb7cff2dbb68a3e3e8b9f02c600687ef0fab
Roman <bashkatovr@mail.ru>
2023-01-12 15:57:30 +0300

    1

---
+++ src/Car.java
@@ -0,0 +1,65 @@
+public class Car {
+public String brand;
+private String model;
+private double engineVolume;
+private String color;
+private int year;
+private String country;
+
+    public Car(String brand, String model, double engineVolume, String color, int year, String country) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.country = country;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setEngineVolume(double engineVolume) {
+        this.engineVolume = engineVolume;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Car lada = new Car();
+    lada.brand = "Lada Granta";
+    lada.year = 2015;
+    lada.coutry= "Russia";
+    lada.color= "желтый";
+    lada.engineVolume = 1.6;
+        System.out.println( " Данное чудо называется" + lada.brand);
+
+
     }
 }
